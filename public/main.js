6c10b46fb6259332f1319a4158c5a328fec7aa94
Garethhhh <gareth96.gw@gmail.com>
2022-04-08 21:28:21 +0000

    About template

--- public/main.js
+++ public/main.js
@@ -17,6 +17,38 @@ function JokeHTML() {
         <a class="waves-effect waves-light btn">Tell another</a>
         </div>
       </main>
-` 
+      `
+      // Save Data
+      const email = document.querySelector("#email");
+      const nameInput = document.querySelector("#name");
+      const message = document.querySelector("#message");
+      const success = document.querySelector("#success");
+
+
+      // Verify Data  
+      function validateForm(){
+          if(!emailIsValid(email.value)){
+              errorNodes[1].innerText = "Please enter a Name";
+              nameInput.classList.add("error-border");
+          }
+
+          .about{
+              width: 100%;
+              padding; 78px 0px;
+          }
+          .about text{
+              width 650px;
+          }
+          .main{
+               width: 1130px;
+               max-width: 95%;
+               margin: 0;
+               display: flex;
+               align-items: clearInterval;
+               justify-content: space-AudioBufferSourceNode;
+
+          }
+      }
+      
 result.innerHTML = html;
 }
