486a2c1a3ec101155bcd1a34a3e96ca7cb975550
EthanSalads1 <leechongethan@gmail.com>
2022-04-08 03:07:18 +0000

    Joke Prototype

--- public/main.js
+++ public/main.js
@@ -1,3 +1,22 @@
 //Josiah Joel
 //Ethan Lee Chong
 //Gareth Walker
+
+document.getElementsById("JokeButton").onclick = function() {JokeHTML()};
+
+function JokeHTML() {
+    console.log("test");
+    let result = document.querySelector ('#result')
+
+    let html = '';
+    html += ` <h1>Lorem Ipsum.........................</h1>
+        <div class = "ReturnHome">
+            <a class="waves-effect waves-light btn">Return Home</a>
+        </div>
+        <div class = "TellAnother">
+        <a class="waves-effect waves-light btn">Tell another</a>
+        </div>
+      </main>
+` 
+result.innerHTML = html;
+}
