9a8d4dadb0ed693361fb8461c831e9c90be59de3
JosiahJoeking <josiahjoelblu5.3@gmail.com>
2022-04-09 01:33:38 +0000

    home page 1st draft

--- public/main.js
+++ public/main.js
@@ -2,21 +2,34 @@
 //Ethan Lee Chong
 //Gareth Walker
 
-document.getElementsById("JokeButton").onclick = function() {JokeHTML()};
-
 function JokeHTML() {
     console.log("test");
     let result = document.querySelector ('#result')
 
     let html = '';
     html += ` <h1>Lorem Ipsum.........................</h1>
-        <div class = "ReturnHome">
+        <div Id = "ReturnHome">
             <a class="waves-effect waves-light btn">Return Home</a>
         </div>
-        <div class = "TellAnother">
+        <div Id = "TellAnother">
         <a class="waves-effect waves-light btn">Tell another</a>
         </div>
       </main>
       `
     result.innerHTML = html;
 }
+
+ // Save Data
+ const email = document.querySelector("#email");
+ const nameInput = document.querySelector("#name");
+ const message = document.querySelector("#message");
+ const success = document.querySelector("#success");
+
+
+ // Verify Data  
+ function validateForm(){
+     if(!emailIsValid(email.value)){
+         errorNodes[1].innerText = "Please enter a Name";
+         nameInput.classList.add("error-border");
+     }
+ }
