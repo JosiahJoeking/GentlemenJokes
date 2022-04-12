baf7aca190b2ab8d42368fc270b9f2244335003f
EthanSalads1 <leechongethan@gmail.com>
2022-04-12 01:58:07 +0000

    AJAX Working

--- public/main.js
+++ public/main.js
@@ -10,41 +10,104 @@ document.addEventListener('DOMContentLoaded', (e)=>{
     }, 2000);
 })
 
-function JokeHTML() {
+function getjoke()
+{
+    const request = new XMLHttpRequest();
+    request.open("GET", "https://v2.jokeapi.dev/joke/Any?safe-mode");
+    request.send();
+    request.onload = () => {
+        if (request.status === 200)
+        {
+            console.log(JSON.parse(request.response));
+            JokeDisplay(JSON.parse(request.response));
+        } else {
+            console.log(request)
+            console.log(`error ${request.status}`)
+        }
+    }
+}
+
+function JokeDisplay(Joke) {
     console.log("test");
     let result = document.querySelector ('#result')
 
+    if (Joke.type === "single")
+    {
+        let html = '';
+ 
+        html += `<h4>Joke:</h4>
+        <h2>${Joke.joke}</h2>
+            <div id = "Button">
+                <a class="waves-effect waves-light btn-large black amber-text" href="home.html">Return Home</a>
+            </div>
+            <div id = "Button">
+                <a class="waves-effect waves-light btn-large amber black-text" onclick="getjoke()">Tell another</a>
+            </div>
+        </main>
+        `
+        result.innerHTML = html;
+    }
+    if (Joke.type === "twopart")
+    {
+    let result = document.querySelector ('#result')
     let html = '';
-    html += ` <h2>Lorem Ipsum.........................</h2>
+ 
+    html += `
+    <h4>Setup:</h4>
+    <h2>${Joke.setup})</h2>
+    <h4>Delivery:</h4>
+    <h2>${Joke.delivery})</h2>
         <div id = "Button">
             <a class="waves-effect waves-light btn-large black amber-text" href="home.html">Return Home</a>
         </div>
         <div id = "Button">
-            <a class="waves-effect waves-light btn-large amber black-text" onclick="JokeHTML()">Tell another</a>
+            <a class="waves-effect waves-light btn-large amber black-text" onclick="getjoke()">Tell another</a>
+        </div>
+    </main>
+    `
+    result.innerHTML = html;
+    }
+}
+
+function getactivity()
+{
+    const request = new XMLHttpRequest();
+    request.open("GET", "https://www.boredapi.com/api/activity");
+    request.send();
+    request.onload = () => {
+        if (request.status === 200)
+        {
+            console.log(JSON.parse(request.response));
+            ToDoHTML(JSON.parse(request.response));
+        } else {
+            console.log(request)
+            console.log(`error ${request.status}`)
+        }
+    }
+}
+
+function ToDoHTML(ToDo) {
+    console.log("test");
+    let result = document.querySelector ('#result')
+    let price;
+    price = ToDo.price * 10;
+
+    let html = '';
+    html += ` <h4>${ToDo.activity}</h4>
+                <h2>Participants: ${ToDo.participants}</h2>
+                <h2>Cost: ${price} / 10 </h2>
+        <div id = "Button">
+            <a class="waves-effect waves-light btn-large black amber-text" href="home.html">Return Home</a>
+        </div>
+        <div id = "Button">
+            <a class="waves-effect waves-light btn-large amber black-text" onclick="getactivity()">Tell another</a>
         </div>
       </main>
       `
     result.innerHTML = html;
 }
 
-function ToDoHTML() {
-    console.log("test");
-    let result = document.querySelector ('#result')
 
-    let html = '';
-    html += ` <h2>Lorem Ipsum.........................</h2>
-                <h4>Participants: ........</h4>
-                <h4>Cost: ........</h4>
-        <div id = "Button">
-            <a class="waves-effect waves-light btn-large black amber-text" href="home.html">Return Home</a>
-        </div>
-        <div id = "Button">
-            <a class="waves-effect waves-light btn-large amber black-text" onclick="ToDoHTML()">Tell another</a>
-        </div>
-      </main>
-      `
-    result.innerHTML = html;
-}
 
  // Save Data
  const email = document.querySelector("#email");
